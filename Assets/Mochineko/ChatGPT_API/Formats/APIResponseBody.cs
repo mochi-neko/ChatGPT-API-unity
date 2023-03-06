27bb3d49ac2bc601d0895ca7f511e9abd93f5446
mochi-neko <t.o.e.4315@gmail.com>
2023-03-06 10:39:57 +0900

    Add required attributes to response body

--- Assets/Mochineko/ChatGPT_API/Formats/APIResponseBody.cs
+++ Assets/Mochineko/ChatGPT_API/Formats/APIResponseBody.cs
@@ -11,23 +11,34 @@ namespace Mochineko.ChatGPT_API.Formats
     [JsonObject]
     public sealed class APIResponseBody
     {
-        [JsonProperty("id")] public string ID { get; private set; } = string.Empty;
-        [JsonProperty("object")] public string Object { get; private set; } = string.Empty;
-        [JsonProperty("created")] public uint Created { get; private set; }
-        [JsonProperty("model")] public string Model { get; private set; } = string.Empty;
-        [JsonProperty("usage")] public Usage Usage { get; private set; } = new();
-        [JsonProperty("choices")] public Choice[] Choices { get; private set; } = Array.Empty<Choice>();
+        [JsonProperty("id"), JsonRequired]
+        public string ID { get; private set; } = string.Empty;
+
+        [JsonProperty("object"), JsonRequired]
+        public string Object { get; private set; } = string.Empty;
+
+        [JsonProperty("created"), JsonRequired]
+        public uint Created { get; private set; }
+
+        [JsonProperty("model"), JsonRequired]
+        public string Model { get; private set; } = string.Empty;
+
+        [JsonProperty("usage"), JsonRequired]
+        public Usage Usage { get; private set; } = new();
+
+        [JsonProperty("choices"), JsonRequired]
+        public Choice[] Choices { get; private set; } = Array.Empty<Choice>();
 
         /// <summary>
         /// Result of chat completion.
         /// </summary>
         public string ResultMessage
             => Choices.Length != 0 ? Choices[0].Message.Content : string.Empty;
-        
+
         public string ToJson()
             => JsonConvert.SerializeObject(this, Formatting.Indented);
 
         public static APIResponseBody? FromJson(string json)
-            => JsonConvert.DeserializeObject<APIResponseBody>(json);
+            => JsonConvert.DeserializeObject<APIResponseBody>(json, new JsonSerializerSettings());
     }
 }
