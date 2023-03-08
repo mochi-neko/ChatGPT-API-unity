f59d69e1de51ca56b2ea4d6d3cbbe1a3d8b1f1c2
mochi-neko <t.o.e.4315@gmail.com>
2023-03-08 10:01:36 +0900

    Add summaries for enums

--- Assets/Mochineko/ChatGPT_API/Formats/Model.cs
+++ Assets/Mochineko/ChatGPT_API/Formats/Model.cs
@@ -1,9 +1,18 @@
 #nullable enable
 namespace Mochineko.ChatGPT_API.Formats
 {
+    /// <summary>
+    /// Chat model.
+    /// </summary>
     public enum Model : byte
     {
+        /// <summary>
+        /// "gpt-3.5-turbo" latest GPT3.5 turbo model.
+        /// </summary>
         Turbo,
+        /// <summary>
+        /// "gpt-3.5-turbo-0301" fixed GPT3.5 turbo model at 03/01/2023.
+        /// </summary>
         Turbo0301,
     }
 }
--- Assets/Mochineko/ChatGPT_API/Formats/Role.cs
+++ Assets/Mochineko/ChatGPT_API/Formats/Role.cs
@@ -3,8 +3,17 @@ namespace Mochineko.ChatGPT_API.Formats
 {
     internal enum Role : byte
     {
+        /// <summary>
+        /// "system"
+        /// </summary>
         System,
+        /// <summary>
+        /// "assistant"
+        /// </summary>
         Assistant,
+        /// <summary>
+        /// "user"
+        /// </summary>
         User,
     }
 }
