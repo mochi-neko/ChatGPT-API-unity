0e0339432d0b7532d19760c96bfdf630010d7be9
mochi-neko <t.o.e.4315@gmail.com>
2023-06-14 11:38:06 +0900

    Update chat models at 06/13

--- Assets/Mochineko/ChatGPT_API/Model.cs
+++ Assets/Mochineko/ChatGPT_API/Model.cs
@@ -1,8 +1,11 @@
 #nullable enable
+using System;
+
 namespace Mochineko.ChatGPT_API
 {
     /// <summary>
     /// Chat model.
+    /// See also https://platform.openai.com/docs/models/model-endpoint-compatibility.
     /// </summary>
     public enum Model : byte
     {
@@ -13,7 +16,20 @@ namespace Mochineko.ChatGPT_API
         /// <summary>
         /// "gpt-3.5-turbo-0301" fixed GPT3.5 turbo model at 03/01/2023.
         /// </summary>
+        [Obsolete("Use Turbo0613 instead of Turbo0301 until 09/13/2023.")]
         Turbo0301,
+        /// <summary>
+        /// "gpt-3.5-turbo-0613" fixed GPT3.5 turbo model at 06/13/2023.
+        /// </summary>
+        Turbo0613,
+        /// <summary>
+        /// "gpt-3.5-turbo-16k" latest GPT3.5 turbo model for 16k tokens.
+        /// </summary>
+        Turbo16K,
+        /// <summary>
+        /// "gpt-3.5-turbo-16k-0613" fixed GPT3.5 turbo model for 16k tokens at 06/13/2023.
+        /// </summary>
+        Turbo16K0613,
         /// <summary>
         /// "gpt-4" latest GPT4 model.
         /// </summary>
@@ -21,7 +37,12 @@ namespace Mochineko.ChatGPT_API
         /// <summary>
         /// "gpt-4-0314", fixed GPT4 model at 03/14/2023.
         /// </summary>
+        [Obsolete("Use Four0613 instead of Four0314 until 09/13/2023.")]
         Four0314,
+        /// <summary>
+        /// "gpt-4-0613", fixed GPT4 model at 06/13/2023.
+        /// </summary>
+        Four0613,
         /// <summary>
         /// "gpt-4-32k", latest GPT4 model for 32k tokens.
         /// </summary>
@@ -29,6 +50,11 @@ namespace Mochineko.ChatGPT_API
         /// <summary>
         /// "gpt-4-32k-0314", fixed GPT4 model for 32k tokens at 03/14/2023.
         /// </summary>
+        [Obsolete("Use Four32K0613 instead of Four32K0314 until 09/13/2023.")]
         Four32K0314,
+        /// <summary>
+        /// "gpt-4-32k-0613", fixed GPT4 model for 32k tokens at 06/13/2023.
+        /// </summary>
+        Four32K0613,
     }
 }
--- Assets/Mochineko/ChatGPT_API/ModelResolver.cs
+++ Assets/Mochineko/ChatGPT_API/ModelResolver.cs
@@ -9,10 +9,15 @@ namespace Mochineko.ChatGPT_API
         {
             [Model.Turbo] = "gpt-3.5-turbo",
             [Model.Turbo0301] = "gpt-3.5-turbo-0301",
+            [Model.Turbo0613] = "gpt-3.5-turbo-0613",
+            [Model.Turbo16K] = "gpt-3.5-turbo-16k",
+            [Model.Turbo16K0613] = "gpt-3.5-turbo-16k-0613",
             [Model.Four] = "gpt-4",
             [Model.Four0314] = "gpt-4-0314",
+            [Model.Four0613] = "gpt-4-0613",
             [Model.Four32K] = "gpt-4-32k",
             [Model.Four32K0314] = "gpt-4-32k-0314",
+            [Model.Four32K0613] = "gpt-4-32k-0613",
         };
 
         public static Model ToModel(this string model)
